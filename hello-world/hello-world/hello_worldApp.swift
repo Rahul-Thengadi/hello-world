dc423a3f0aca84b92b71f8d344db41256c40eec9
Rahul <thengadir@gmail.com>
2022-12-26 01:44:31 +0530

    intial commit

---
+++ hello-world/hello-world/hello_worldApp.swift
@@ -0,0 +1,17 @@
+//
+//  hello_worldApp.swift
+//  hello-world
+//
+//  Created by Rahul Thengadi on 26/12/22.
+//
+
+import SwiftUI
+
+@main
+struct hello_worldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
